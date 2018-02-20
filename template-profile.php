7791cf0c9512790dcc7330d8ae6b71a0bbc71a73
rilinda1987 <rilinda.jahiu@kolabor.net>
2018-02-20 15:12:18 +0100

    rilinda 7

--- template-profile.php
+++ template-profile.php
@@ -7,15 +7,14 @@
 get_header(); ?>
 
 <?php get_template_part('pageheader'); ?>
-	
+ 
 
 <div class="site-body site-pad">
-	<div class="site-container loans form_box">
+ <div class="site-container loans form_box">
   
 <?php 
 /*User Login programaticly starts here*/
 if(isset($_SESSION['username'])) {
-<<<<<<< HEAD
 
 
     $user_login     = esc_attr($_SESSION['username']);
@@ -26,51 +25,7 @@ if(isset($_SESSION['username'])) {
     'user_login'    =>      $user_login,
     'user_pass'     =>      $user_password,
     'user_email'    =>      $user_email,
-    'role'          =>      'Administrator'
-);
-
-$creds = array();
-$creds['user_login'] = $user_login;
-$creds['user_password'] = $user_password;
-$creds['remember'] = true;
-
-$user = wp_signon( $creds, false );
-
-$userID = $user->ID;
-
-wp_set_current_user( $userID, $user_login );
-wp_set_auth_cookie( $userID, true, false );
-do_action( 'wp_login', $user_login );
-
-if ( is_user_logged_in() ) : echo 'SUCCESS'; ?>
-echo $userID."<br>";
-<h1>Html for logged in user </h1>
-<?php else : echo 'FAIL!'; ?>
-=======
->>>>>>> c93ee2cc6ab1aca0c09ebdfcec57f8b93f936e6e
-
-<?php endif; 
-
-}
-else
-{
-
-/*If session is not set, and uses is not loged in redirect to home page*
-/* Developers find a code from KOLABOR */
-  
-
-}
-/*User Login programaticly ends here*/
-
-    $user_login     = esc_attr($_SESSION['username']);
-    $user_password  = esc_attr($_SESSION['password']);
-    $user_email     = esc_attr($_SESSION['username']);
-
-    $user_data = array(
-    'user_login'    =>      $user_login,
-    'user_pass'     =>      $user_password,
-    'user_email'    =>      $user_email,
-    'role'          =>      'Administrator'
+    'role'          =>      'Author'
 );
 
 $creds = array();
@@ -91,7 +46,7 @@ echo $user_login. "<br>"; ?>
 <h1><b>KOLABOR</b></h1>
 <?php else : echo 'FAIL!'; ?>
 
-<?php endif; 
+<?php endif;
 
 }
 else
@@ -109,6 +64,6 @@ else
 ?>
                
  
-	</div>
-</div>
+ </div>
+</div> 
 <?php get_footer(); ?>
