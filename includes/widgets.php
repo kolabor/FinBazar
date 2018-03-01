6b4cee4226d25bb4b90101dd79d421fce9195510
rilinda1987 <rilinda.jahiu@kolabor.net>
2018-03-01 09:44:18 +0100

    first commit

--- includes/widgets.php
+++
@@ -1,5 +0,0 @@
-<?php
-include get_template_directory().'/includes/widgets/alba-carousel.php';
-include get_template_directory().'/includes/widgets/alba-heading.php';
-include get_template_directory().'/includes/widgets/alba-icon.php';
-?>
